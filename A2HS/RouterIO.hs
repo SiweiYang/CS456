6cffef7de1c88a54a34a293cfa4c8d5db1ab4dfe
Siwei Yang <yangsiwei880813@gmail.com>
2013-07-29 21:46:28 -0400

    adding comments

--- A2HS/RouterIO.hs
+++ A2HS/RouterIO.hs
@@ -6,6 +6,7 @@ import System.Environment (getArgs)
 import System.IO.Error (catch, IOError)
 import Data.Maybe (Maybe(..), maybeToList, catMaybes)
 
+-- read available edges and their cost from the specified edge file
 readEdges :: String -> String -> IO [RouteEdge]
 readEdges filename root = do
                             content <- readFile filename
@@ -21,12 +22,15 @@ readEdges filename root = do
                             constructor (v1:v2:cost:[])= [(RouteEdge v1 v2 (read cost)), (RouteEdge v2 v1 (read cost))]
                             constructor []= []
 
+-- 
 proposeEdge :: [RoutePath] -> RouteEdge -> [RoutePath]
 proposeEdge paths (RouteEdge v1 v2 ecost) = filter (\(RoutePath path pcost) -> head path == v1) path'
                                             where
                                               applyEdge (RoutePath path pcost) = if head path == v2 then RoutePath (v1:path) (pcost + ecost) else RoutePath path pcost
                                               path' = map applyEdge paths
 
+-- read paths from the specified table file
+-- silently return empty list when file not exists
 readPaths :: String -> IO [RoutePath]
 readPaths v = catch readPath' (\e -> print e >> return [])
               where
@@ -42,14 +46,24 @@ readPaths v = catch readPath' (\e -> print e >> return [])
                 constructor (vd:cost:vs) = Just (RoutePath ((v:vs) ++ [vd]) (read cost))
                 constructor []= Nothing
 
+-- deduplication of paths so that for any destination there will be at most one path
+-- shortest path out-run others for the same destination
+-- if there is a tie, then the name of the starting node on the path is used to break the tie consistently
 cleanPaths :: [RoutePath] -> [RoutePath]
 cleanPaths paths = foldr (\p paths -> updatePath paths p) [] paths
                    where
                      updatePath :: [RoutePath] -> RoutePath -> [RoutePath]
                      updatePath [] p = [p]
-                     updatePath (p:paths) p' = if (head (path p)) == (head (path p')) && (last (path p)) == (last (path p')) then if (pcost p) > (pcost p') then p':paths else p:paths else p:(updatePath paths p')
+                     updatePath (p:paths) p' = if (head (path p)) == (head (path p')) && (last (path p)) == (last (path p'))
+                                               then case of compare (pcost p) > (pcost p')
+                                                    GT -> p':paths
+                                                    LT -> p:paths
+                                                    EQ -> if head (path p)) > (head (path p') then p':paths else p:paths
+                                               else p:(updatePath paths p')
                      
 
+-- write paths to the specified table file
+-- silently return empty list when file not exists
 writePaths :: String -> [RoutePath] -> IO ()
 writePaths v paths = do
                       let out = if length paths > 0 then content else ""
@@ -58,5 +72,6 @@ writePaths v paths = do
                       p:ps = paths
                       content = foldr (\path out -> out ++ "\n" ++ (show path)) (show p) ps
 
+-- filter out all paths that came across the specified node
 excludeNode :: [RoutePath] -> String -> [RoutePath]
 excludeNode paths v = filter (\(RoutePath path pcost) -> notElem v (tail path)) paths
